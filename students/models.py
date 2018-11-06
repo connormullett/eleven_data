9b33f6cc5f13fe37dac7cd7b3c337172e38021f0
Connor <connormullett@gmail.com>
2018-11-06 16:01:21 -0500

    initial

---
+++ students/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+
+class Student(models.Model):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    enrolled_at = models.DateTimeField(default=datetime.now(), blank=True)
+    gpa = models.IntegerField()
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/connor/Documents/PythonProjects/eleven_data/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
